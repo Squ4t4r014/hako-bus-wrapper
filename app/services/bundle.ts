62dae7fe008cc68b3c96a1178d5d01e8156cda16
Squatarola <b1017150@fun.ac.jp>
2021-11-08 22:01:53 +0900

    コードを書き始めた

--- app/services/bundle.ts
+++ app/services/bundle.ts
@@ -4,5 +4,9 @@ import "bootstrap-honoka/dist/css/bootstrap.min.css"
 import "animate.css"
 import "./style.scss"
 
-(function () {
-})();
+//For Android PWA
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("sw.js").then((req) => {
+        console.log("Service worker registerd.", req)
+    });
+}
---
+++ app/services/sw.js
@@ -0,0 +1,20 @@
+var CACHE_NAME = "pwa-hakobus-wrapper-caches"
+var urlsToCache = [
+    "",
+]
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(urlsToCache)
+        })
+    )
+})
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response ? response : fetch(event.request)
+        })
+    )
+})
